edaa9d7e1a341382200719aaa6a7f1d4528bb3ee
RagDollPeach <kudrad26@gmail.com>
2022-07-22 13:23:00 +0300

    lesson_7 вынес создание объекта ретрофита в MyApplication

--- app/src/main/java/com/example/weather/MyApplication.kt
+++ app/src/main/java/com/example/weather/MyApplication.kt
@@ -2,8 +2,12 @@ package com.example.weather
 
 import android.app.Application
 import androidx.room.Room
+import com.example.weather.model.retrofit.WeatherApi
 import com.example.weather.model.room.WeatherDatabase
 import com.example.weather.utils.ROOM_DATABASE
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MyApplication : Application() {
     override fun onCreate() {
@@ -15,6 +19,7 @@ class MyApplication : Application() {
         private var myApplication: MyApplication? = null
         private var weatherDatabase: WeatherDatabase? = null
         fun getMyApp() = myApplication!!
+        val retrofit = retrofitCreate()
 
         fun getWeatherDatabase(): WeatherDatabase {
             if (weatherDatabase == null) {
@@ -24,6 +29,14 @@ class MyApplication : Application() {
                         .build()
             }
             return weatherDatabase!!
+        }
+
+        private fun retrofitCreate(): WeatherApi {
+            val retrofitImpl = Retrofit.Builder()
+            retrofitImpl.baseUrl("https://api.weather.yandex.ru")
+            retrofitImpl.addConverterFactory(
+                GsonConverterFactory.create(GsonBuilder().setLenient().create()))
+            return retrofitImpl.build().create(WeatherApi::class.java)
         }
     }
 }
--- app/src/main/java/com/example/weather/model/retrofit/RepoDetailsRetrofitImpl.kt
+++ app/src/main/java/com/example/weather/model/retrofit/RepoDetailsRetrofitImpl.kt
@@ -1,28 +1,22 @@
 package com.example.weather.model.retrofit
 
 import com.example.weather.BuildConfig
+import com.example.weather.MyApplication
 import com.example.weather.domain.Weather
 import com.example.weather.model.MyLargeFatCallBack
 import com.example.weather.model.RepositoryDetails
 import com.example.weather.model.dto.WeatherDTO
 import com.example.weather.utils.convertDtoToModel
-import com.google.gson.GsonBuilder
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
 
 class RepoDetailsRetrofitImpl: RepositoryDetails {
 
     override fun getWeather(weather: Weather, callBack: MyLargeFatCallBack) {
-        val retrofitImpl = Retrofit.Builder()
-        retrofitImpl.baseUrl("https://api.weather.yandex.ru")
-        retrofitImpl.addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
-        val api = retrofitImpl.build().create(WeatherApi::class.java)
        // api.getWeather(BuildConfig.WEATHER_API_KEY,lat,lon).execute()
-        api.getWeather(BuildConfig.WEATHER_API_KEY,weather.city.lat,weather.city.lon).enqueue(object : Callback<WeatherDTO> {
+        MyApplication.retrofit.getWeather(BuildConfig.WEATHER_API_KEY,weather.city.lat,weather.city.lon).enqueue(object : Callback<WeatherDTO> {
             override fun onResponse(call: Call<WeatherDTO>, response: Response<WeatherDTO>) {
                 if (response.isSuccessful && response.body() != null) {
                     callBack.onResponse(convertDtoToModel(response.body()!!,weather))
